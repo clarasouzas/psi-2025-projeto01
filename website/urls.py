19c1ae0153c08de160bccdbc61672459fd606497
Clara Souza <clarasouzacs07@gmail.com>
2025-07-05 18:10:03 -0300

    caminhos views/urls e inicio do html

---
+++ website/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index, name="index"),
+    path('about.html/',views.about, name="about"),
+    path('contact.html/',views.contact, name="contact"),
+    path('doctor.html/',views.doctor, name="doctor"),
+
+
+]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+def index(requests):
+    return render(requests , "website/index.html")
+def about(requests):
+    return render(requests , "website/about.html")
+def contact(requests):
+    return render(requests , "website/contact.html")
+def doctor(requests):
+    return render(requests , "website/doctor.html")
+
 # Create your views here.
