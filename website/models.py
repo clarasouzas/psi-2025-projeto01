78367123e1161a6330f22cb3d670094510afa2fe
Clara Souza <clarasouzacs07@gmail.com>
2025-09-08 14:27:42 -0300

    projeto02-inicio

--- website/models.py
+++ website/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Ator(models.Model):
+    nome = models.CharField(max_length=200)
+    apelido = models.CharField(max_length=200)
+    especialidade = models.CharField(max_length=200)
+    descricao = models.TextField()
+    imagem = models.ImageField(upload_to='personagem/', null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+    
+class AtorImagem(models.Model):
+    ator = models.ForeignKey(Ator, on_delete=models.CASCADE, related_name='imagens')
+    imagem = models.ImageField(upload_to='atores_galeria/')
+    legenda = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return f"Imagem de {self.ator.nome}"
+    
+class ConteudoPagina(models.Model):
+    PAGINAS = [
+        ('inicio', 'Início'),
+        ('sobre', 'Sobre'),
+        
+    ]
+    pagina = models.CharField(max_length=20, choices=PAGINAS, unique=True)
+    titulo = models.CharField(max_length=200)
+    subtitulo = models.CharField(max_length=300, blank=True)
+    texto = models.TextField(blank=True)
+    imagem_header = models.ImageField(upload_to='banner/', null=True, blank=True)
+
+    def __str__(self):
+        return self.pagina
+
+
+
+class App(models.Model):
+    titulo = models.CharField("Título do site", max_length=200)
+
+    instagram = models.URLField("Instagram", blank=True)
+    youtube = models.URLField("YouTube", blank=True)
+    x = models.URLField("X (Twitter)", blank=True)
+    facebook = models.URLField("Facebook", blank=True)
+    linkedin = models.URLField("LinkedIn", blank=True)
+    tiktok = models.URLField("TikTok", blank=True)
+
+    copyright = models.CharField("Copyright", max_length=100, default="Seu Nome")
+    ano = models.PositiveIntegerField("Ano", default=datetime.now().year)
+
+    def __str__(self):
+        return "Configurações do Site"
--- website/urls.py
+++ website/urls.py
@@ -2,10 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name="index"),
-    path('elenco/<int:id_post>/',views.elenco, name="elenco"),
-    path('elenco/', views.elenco_list, name='elenco_list'),
-    path('sobre/',views.sobre, name="sobre"),
-
-
+    path('', views.index, name='index'),
+    path('sobre/', views.sobre, name='sobre'),
+    path('elenco/', views.elenco, name='elenco'),
+    path('ator/<int:id_ator>/', views.ator, name='ator'),
 ]
